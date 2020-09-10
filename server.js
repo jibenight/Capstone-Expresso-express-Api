9157195d78e7c1c86147afbf37f9c8bd4cfc5a90
Jean <nguyen.jean@me.com>
2020-09-10 15:30:00 +0200

    add server.js and api/api.js; setup express.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const express = require('express');
+const morgan = require('morgan');
+
+const apiRouter = require('./api/api');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// routes
+app.use('/api', apiRouter);
+
+// handling error
+app.use(errorhandler());
+
+app.listen(PORT, () => {
+  console.log(`Server is ready at http://localhost:${PORT}`);
+});
+
+module.exports = app;
